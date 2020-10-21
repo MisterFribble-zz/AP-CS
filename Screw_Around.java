80c4f56b31341fb55b3476df44b9cc5dd265ca79
Andrew Gessel <andygessel@gmail.com>
2020-10-21 19:25:15 +0000

    Add Screw Around file

---
+++ Screw_Around.java
@@ -0,0 +1,11 @@
+/**
+ * Screw_Around
+ */
+import edhesive.shapes.*;
+
+public class Screw_Around {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+        System.out.println(c);
+    }
+}
