bd83c0d860bb6935c3716bdeda036f1e95617814
Andrew Gessel <andygessel@gmail.com>
2020-10-22 17:20:15 +0000

    Remove activities from IDE branch

--- Unit 2/U2_L2_Activity_Two.java
+++
@@ -1,5 +0,0 @@
-public class U2_L2_Activity_Two {
-    public static void main(String[] args) {
-        System.out.println("\"I do not fear computers. I fear the lack of them.\"\nIsaac Asimov");
-    }
-}
