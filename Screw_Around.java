fb341f680f86af38509b070530cb58357e2937f9
Andrew Gessel <andygessel@gmail.com>
2021-01-14 18:14:10 +0000

    Fix formatting

--- Screw_Around.java
+++ Screw_Around.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import edhesive.shapes.*;
 
 public class Screw_Around {
-    public static void main(String[] args) {
-
-    }
+  public static void main(String[] args) {
+    
+  }
 }
